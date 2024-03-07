e1cb8e162eb02a683dc99da17e7ea2e6747e173e
naormoses <naor.moses@gmail.com>
2024-03-07 13:38:53 +0200

    new files commited

---
+++ index.js
@@ -0,0 +1,29 @@
+
+const express = require('express')
+const cors = require('cors')
+const { request } = require('needle')
+const rateLimit = require ('express-rate-limit')
+require ('dotenv').config()
+
+const PORT = process.env.PORT || 5000
+const app = express()
+
+//Rate limiting
+const limiter = rateLimit({
+    windowMs : 10*60*1000,  //10 minutes
+    max: 5
+})
+
+app.use(limiter)
+app.set('trust proxy', 1)
+
+//Set static folder
+app.use(express.static('public'))
+
+
+//Routes
+app.use('/api', require('./routes'))
+
+//Enable cors
+app.use(cors())
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
